348ed923dd7bbd8f0c1938f501f3cdcdcd303a8c
shahid <shahidthaha2005@gmail.com>
2024-10-18 09:07:09 +0530

    working

--- login.py
+++ login.py
@@ -1,11 +1,31 @@
 import tkinter
+import sqlite3
+# con=sqlite3.connect("tkinder_data.db")
+# con.execute('create table sample (uname text,password text)')
 win=tkinter.Tk()
 win.title('login')
 win.configure(bg="grey")
 win.minsize(400,400)
 win.maxsize(500,500)
+def home():
+    win2=tkinter.Tk()
+    l1=tkinter.Label(win2,text="Home Page",bg='green',fg='blue')
+    l1.place(x=150,y=20)
+    btn1=tkinter.Button(win2,text="log out",command=win2.quit)
+    btn1.place(x=200,y=0)
+    win2.mainloop()
 
 
+def login():
+    con=sqlite3.connect("tkinder_data.db")
+    data=con.execute("select * from sample")
+    f=0
+    for i in data:
+        if i[0]==e1.get() and i[1]==e2.get():
+            f=1
+            home()
+    if f==0:
+        l3.config(text="invalid username or password")
 
 def reg_form():
     win1=tkinter.Tk()
@@ -13,6 +33,12 @@ def reg_form():
     win1.configure(bg="green")
     win1.minsize(400,400)
     win1.maxsize(500,500)
+
+    def reg():
+        con=sqlite3.connect("tkinder_data.db")
+        con.execute("insert into sample(uname,password)values(?,?)",(e3.get(),e4.get()))
+        con.commit()
+        win1.destroy()
 
     l3=tkinter.Label(win1,text="register",bg='green',fg="white")
     l3.place(x=150,y=20)
@@ -24,17 +50,17 @@ def reg_form():
     e3.place(x=150,y=50)
     e4=tkinter.Entry(win1)
     e4.place(x=150,y=80)
-    l7=tkinter.Label(win1,text="phone no")
-    l7.place(x=75,y=100)
-    e5=tkinter.Entry(win1)
-    e5.place(x=150,y=110)
+    # l7=tkinter.Label(win1,text="phone no")
+    # l7.place(x=75,y=100)
+    # e5=tkinter.Entry(win1)
+    # e5.place(x=150,y=110)
 
     
     
     
 
 
-    btn3=tkinter.Button(win1,text="Submmit",bg="gray",activebackground='blue',activeforeground='black',padx=10,pady=8,command="save")
+    btn3=tkinter.Button(win1,text="Submmit",bg="gray",activebackground='blue',activeforeground='black',padx=10,pady=8,command=reg)
     btn3.place(x=200,y=150)
     win1.mainloop()
 
